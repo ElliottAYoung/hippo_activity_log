e6baf926b3215cfc80c6ee3aa314af3bcbf0efc2
ElliottAYoung <elliott.a.young@gmail.com>
2018-06-01 08:47:46 -0500

    bump to version 0.2.0

--- db/migrate/20180601134515_add_loan_application_data_tracking_to_events.rb
+++ db/migrate/20180601134515_add_loan_application_data_tracking_to_events.rb
@@ -1,6 +1,6 @@
 class AddLoanApplicationDataTrackingToEvents < ActiveRecord::Migration
   def change
-    add_column :loan_application, :jsonb, default: {}
-    add_column :loan_application_id, :integer, index: true
+    add_column :hippo_activity_log_events, :loan_application, :jsonb, default: {}
+    add_column :hippo_activity_log_events, :loan_application_id, :integer, index: true
   end
 end
--- lib/hippo_activity_log/version.rb
+++ lib/hippo_activity_log/version.rb
@@ -1,3 +1,3 @@
 module HippoActivityLog
-  VERSION = '0.1.2'
+  VERSION = '0.2.0'
 end
